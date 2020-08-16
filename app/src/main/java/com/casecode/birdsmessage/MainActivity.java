eec8a5fbb9e79d4300d0233f2e4b0dc70e205b34
Abdelaziz Daoud <abdelaz9z@icloud.com>
2020-08-16 23:46:56 +0200

    Initial commit

---
+++ app/src/main/java/com/casecode/birdsmessage/MainActivity.java
@@ -0,0 +1,14 @@
+package com.casecode.birdsmessage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
